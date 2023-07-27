e24978df873aabae825c5f197b3000aa0376f99d
hrhfourtyseven <40234906+hrhfourtyseven@users.noreply.github.com>
2023-07-27 19:03:35 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x9b63699a5be657dbcd0ecbbdb4ceea5992a8ba17";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
